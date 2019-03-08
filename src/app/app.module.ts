21d84b37dd9219a42840b2e6688d72f2756b0f68
v_prot01 <v_prot01@uni-muenster.de>
2019-03-08 12:17:06 +0100

    angular cli project imported. added bootstrap4 and ngx-bootstrap.

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import {BsDropdownModule} from 'ngx-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BsDropdownModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
