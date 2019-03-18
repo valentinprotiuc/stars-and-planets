9c45305d6570d9aa2d905f151aaff5f87db7bceb
v_prot01 <v_prot01@uni-muenster.de>
2019-03-18 14:45:21 +0100

    playing around with mongodb

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const mongodb = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 const app = express();
 
-let uri = 'mongodb://heroku_3h2xwfxr:spmc4d27eot7nc4qmgokqijuvf@ds215633.mlab.com:15633/heroku_3h2xwfxr';
+const uri = 'mongodb://heroku_3h2xwfxr:spmc4d27eot7nc4qmgokqijuvf@ds215633.mlab.com:15633/heroku_3h2xwfxr';
 
-let seedData;
+const dbName = 'heroku_3h2xwfxr';
+
+
+const client = new MongoClient(uri);
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/stars-and-planets'));
@@ -17,7 +21,19 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/stars-and-planets/index.html'));
 });
 
-mongodb.MongoClient.connect(uri, function (err, client) {
+client.connect(function (err) {
+  assert.equal(err, null);
+  console.log('Connected correctly to the server.');
+
+  const db = client.db(dbName);
+
+  findAllDocuments(db, function () {
+    client.close();
+  })
+
+});
+
+/*mongodb.MongoClient.connect(uri, function (err, client) {
 
   if (err) throw err;
 
@@ -27,7 +43,17 @@ mongodb.MongoClient.connect(uri, function (err, client) {
 
   console.log(stars);
 
-});
+});*/
+
+const findAllDocuments = function (db, callback) {
+  const collection = db.collection('stars');
+  collection.find({}).toArray(function (err, docs) {
+    assert.equal(err, null);
+    console.log('Records found.');
+    console.log(docs);
+    callback(docs);
+  });
+};
 
 
 // Start the app by listening on the default Heroku port
