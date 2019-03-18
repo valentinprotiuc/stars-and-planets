991db58069e52fabbc51ef8778a5c68da738aee5
v_prot01 <v_prot01@uni-muenster.de>
2019-03-18 13:40:05 +0100

    retrieving data from the server

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const mongodb = require('mongodb');
 
 const app = express();
+
+let uri = 'mongodb://heroku_3h2xwfxr:spmc4d27eot7nc4qmgokqijuvf@ds215633.mlab.com:15633/heroku_3h2xwfxr';
+
+let seedData;
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/stars-and-planets'));
@@ -11,6 +16,27 @@ app.get('/*', function (req, res) {
 
   res.sendFile(path.join(__dirname + '/dist/stars-and-planets/index.html'));
 });
+
+mongodb.MongoClient.connect(uri, function (err, client) {
+
+  if (err) throw err;
+
+  let db = client.db('starsdb');
+
+  let stars = db.collection('stars');
+
+  stars.forEach(function (item) {
+    if (item != null) {
+      seedData.push(item);
+    }
+    console.log(seedData);
+  }, function (err) {
+    console.log(err);
+    db.close();
+  });
+
+});
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
