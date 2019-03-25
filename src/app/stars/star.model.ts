4e8b3eadd98e59cdfddf12fb43c9b7ae8ad4398a
v_prot01 <v_prot01@uni-muenster.de>
2019-03-25 23:36:02 +0100

    fix

--- src/app/stars/star.model.ts
+++ src/app/stars/star.model.ts
@@ -2,9 +2,13 @@ import {Planet} from '../planets/planet.model';
 
 export class Star {
 
-  constructor(public id: string, public name: string, public spectralType: string, public solarMass: number,
+  constructor(private _id: string, public name: string, public spectralType: string, public solarMass: number,
               public solarRadius: number, public effectiveTemperature: number,
               public distance: number, public orbitingPlanets: Planet[]) {
+  }
+
+  get id(): string {
+    return this._id;
   }
 
 }
