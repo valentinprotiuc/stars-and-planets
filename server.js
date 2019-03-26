42d292adcb493ec87f4f7cdea8d81bd0be203b87
v_prot01 <v_prot01@uni-muenster.de>
2019-03-26 16:52:59 +0100

    fix

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.get('/data', (req, res) => {
 
 app.post('/data', (req, res) => {
   console.log('Add req body: ', req.body);
-  db.collection('stars').inserOne({name : 'nananame'}, (error, result) => {
-    if (error) console.log("Fuck me: ", error);
+  db.collection('stars').inserOne({"name" : "nananame"}, (error, result) => {
+    if (error) throw error;
     db.collection('stars').find({}).toArray((error, result) => {
       if (error) throw err;
       res.send(result);
