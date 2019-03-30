7aa2d4d91d9f2dc0b1110e3bd3ba68f291dc8ea2
v_prot01 <v_prot01@uni-muenster.de>
2019-03-30 01:54:51 +0100

    fix

--- api/models/db.js
+++ api/models/db.js
@@ -1,9 +1,6 @@
-var mongoose = require('mongoose');
-var gracefulShutdown;
-var dbURI = 'mongodb://heroku_3h2xwfxr:spmc4d27eot7nc4qmgokqijuvf@ds215633.mlab.com:15633/heroku_3h2xwfxr';
-/*if (process.env.NODE_ENV === 'production') {
-  dbURI = process.env.MONGOLAB_URI;
-}*/
+const mongoose = require('mongoose');
+let gracefulShutdown;
+const dbURI = 'mongodb://heroku_3h2xwfxr:spmc4d27eot7nc4qmgokqijuvf@ds215633.mlab.com:15633/heroku_3h2xwfxr';
 
 mongoose.connect(dbURI);
 
