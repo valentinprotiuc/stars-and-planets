0115551da2d8ca4b8d5e3d45831c45b2cd909b51
v_prot01 <v_prot01@uni-muenster.de>
2019-03-30 11:18:41 +0100

    fix

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -29,6 +29,7 @@ export class ServerService {
   }
 
   updateStarInDB(star: Star, initialName: string) {
+    console.log('Star: ', star);
     return this.http.post('https://stars-and-planets.herokuapp.com/data', {star, initialName});
   }
 
