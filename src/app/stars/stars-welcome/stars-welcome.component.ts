65843c152450c06ea3688d46576d3bbb9d2fc980
v_prot01 <v_prot01@uni-muenster.de>
2019-03-31 13:16:18 +0200

    Different text displayed depending on if the user is logged in or not.

--- src/app/stars/stars-welcome/stars-welcome.component.ts
+++ src/app/stars/stars-welcome/stars-welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from '../../authentication.service';
 
 @Component({
   selector: 'app-stars-welcome',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarsWelcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthenticationService) { }
 
   ngOnInit() {
   }
