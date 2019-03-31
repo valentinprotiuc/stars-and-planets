725615f3cec916783a402fb71c3ce93a07f233c5
v_prot01 <v_prot01@uni-muenster.de>
2019-03-31 11:02:47 +0200

    buttons to navigate to the signin and signup pages

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {AuthenticationService, TokenPayload} from '../../authentication.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -8,12 +10,30 @@ import {NgForm} from '@angular/forms';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  credentials: TokenPayload = {
+    email: '',
+    name: '',
+    password: ''
+  };
+
+  constructor(private auth: AuthenticationService, private router: Router) {
+  }
 
   ngOnInit() {
   }
 
   onSignin(form: NgForm) {
 
+    this.credentials.email = form.value.email;
+    this.credentials.name = form.value.name;
+    this.credentials.password = form.value.password;
+    this.auth.login(this.credentials).subscribe(
+      () => {
+        this.router.navigateByUrl('/profile');
+      },
+      (error) => {
+        throw error;
+      },
+    );
   }
 }
