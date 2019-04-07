82458a161d6480b17027dd2140039be6c6b239f6
v_prot01 <v_prot01@uni-muenster.de>
2019-04-07 02:00:09 +0200

    modified the design of signin and signup using MDB

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {NgForm} from '@angular/forms';
+import {FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms';
 import {AuthenticationService, TokenPayload} from '../../authentication.service';
 import {Router} from '@angular/router';
 
@@ -10,23 +10,31 @@ import {Router} from '@angular/router';
 })
 export class SigninComponent implements OnInit {
 
+  validationForm: FormGroup;
+
   credentials: TokenPayload = {
     email: '',
     name: '',
     password: ''
   };
 
-  constructor(private auth: AuthenticationService, private router: Router) {
+  constructor(private auth: AuthenticationService, private router: Router, public fb: FormBuilder) {
+    this.validationForm = fb.group({
+      emailFormEx: [null, [Validators.required, Validators.email]],
+      passwordFormEx: [null, Validators.required],
+    });
   }
+
+  get emailFormEx() { return this.validationForm.get('emailFormEx'); }
+  get passwordFormEx() { return this.validationForm.get('passwordFormEx'); }
 
   ngOnInit() {
   }
 
-  onSignin(form: NgForm) {
+  onSubmit() {
 
-    this.credentials.email = form.value.email;
-    this.credentials.name = form.value.name;
-    this.credentials.password = form.value.password;
+    this.credentials.email = this.validationForm.value.email;
+    this.credentials.password = this.validationForm.value.password;
     this.auth.login(this.credentials).subscribe(
       () => {
         this.router.navigateByUrl('/profile');
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {NgForm} from '@angular/forms';
+import {FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms';
 import {AuthenticationService, TokenPayload} from '../../authentication.service';
 import {Router} from '@angular/router';
 
@@ -10,22 +10,33 @@ import {Router} from '@angular/router';
 })
 export class SignupComponent implements OnInit {
 
+  validationForm: FormGroup;
+
   credentials: TokenPayload = {
     email: '',
     name: '',
     password: ''
   };
 
-  constructor(private auth: AuthenticationService, private router: Router) {
+  constructor(private auth: AuthenticationService, private router: Router, public fb: FormBuilder) {
+    this.validationForm = fb.group({
+      nameFormEx: [null, Validators.required],
+      emailFormEx: [null, [Validators.required, Validators.email]],
+      passwordFormEx: [null, Validators.required]
+    });
   }
+
+  get nameFormEx() { return this.validationForm.get('nameFormEx'); }
+  get emailFormEx() { return this.validationForm.get('emailFormEx'); }
+  get passwordFormEx() { return this.validationForm.get('passwordFormEx'); }
 
   ngOnInit() {
   }
 
-  onSignup(form: NgForm) {
-    this.credentials.email = form.value.email;
-    this.credentials.name = form.value.name;
-    this.credentials.password = form.value.password;
+  onSubmit() {
+    this.credentials.name = this.validationForm.value.name;
+    this.credentials.email = this.validationForm.value.email;
+    this.credentials.password = this.validationForm.value.password;
     this.auth.register(this.credentials).subscribe(
       () => {
         this.router.navigateByUrl('/profile');
